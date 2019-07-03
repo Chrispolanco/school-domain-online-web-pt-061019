dea9459738c9b871e94e838318b9981456023003
Christian Polanco <chrispolanco@sbcglobal.net>
2019-07-03 03:47:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,33 +1,19 @@
 class School
+  attr_accessor :name, :roster
 
   def initialize(name)
     @name = name
     @roster = {}
   end
 
-  def name=(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
-
-  def roster=(roster)
-    @roster = roster
-  end
-
-  def roster
-    @roster
-  end
-
   def add_a_student(name, grade)
-    if  roster[grade]
+    if roster.has_key?(grade)
+      roster[grade] = []
       roster[grade] << name
     else
       roster[grade] = []
       roster[grade] << name
     end
-  end 
+  end
   
 end
